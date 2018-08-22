69fed764cb6f49bf3b3a01a457dfeae547944f1f
gretschann <ag@netwunder.de>
2018-08-22 12:30:47 +0200

    added pnbi-datatable-plus initial components

---
+++ src/PnbiTablePlus.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="pnbi-datatable-plus">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'pnbi-datatable-plus',
+    props: [ 'placeholder' ],
+    mounted () {
+        console.log('adsasd')
+    },
+}
+</script>
