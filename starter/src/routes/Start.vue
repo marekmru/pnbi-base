b8021b1cc8d6e5f77205dd80ed357ca42afc626b
gretschann <ag@netwunder.de>
2018-08-27 16:03:41 +0200

    removed unused code

--- starter/src/routes/Start.vue
+++ starter/src/routes/Start.vue
@@ -14,24 +14,13 @@
 
       <!-- default slot -->
         <pnbi-datatable-plus
-          :items="items" :headers="headers" :loading="loading"               tableIdentifier="123" :total-items="totalItems" @padinationEvent="pagination_test">
+          :items="items" :headers="headers" :loading="loading"               tableIdentifier="123" :total-items="totalItems" @padinationEvent="onPagination">
           <tr slot="row" slot-scope="props">
             <td>{{props.props.item.name}}</td>
             <td>{{props.props.item.age}}</td>
             <td>{{props.props.item.value}}</td>
           </tr>
         </pnbi-datatable-plus>
-
-        <!-- <v-data-table :headers="filteredHeaders"
-        :items="items"
-        :rows-per-page-items="[10,25,50, {'text':'Alle','value':-1}]" rows-per-page-text="Elemente pro Seite"
-        :loading="!items" class="mytable">
-        <template slot="items" slot-scope="props">
-          <td v-if="show('name')">{{props.item.name}}</td>
-          <td v-if="show('age')">{{props.item.age}}</td>
-          <td v-if="show('value')">{{props.item.value}}</td>
-        </template>
-        </v-data-table> -->
 
     </pnbi-datatable>
 
@@ -45,12 +34,12 @@ export default {
   // mixins: [TablePlusMixin],
   mounted () {
     console.log(config)
-    this.getDataFromApi()
-      .then(data => {
-        console.log('data', data)
-        this.items = data.items
-        this.totalItems = data.totalItems
-      })
+    // this.getDataFromApi()
+    //   .then(data => {
+    //     console.log('data', data)
+    //     this.items = data.items
+    //     this.totalItems = data.totalItems
+    //   })
   },
   props: {
     propName: {
@@ -61,9 +50,9 @@ export default {
   watch: {
     pagination: {
       handler () {
-        console.log('getdata')
         this.getDataFromApi()
           .then(data => {
+            console.log('pagination event', data)
             this.items = data.items
             this.totalItems = data.totalItems
           })
@@ -90,45 +79,61 @@ export default {
     }
   },
   methods: {
-    pagination_test (data, event) {
-      console.log('pagi', data)
+    onPagination (data, event) {
       this.pagination = data
     },
     getDataFromApi () {
       this.loading = true
       return new Promise((resolve, reject) => {
         let items = [
-          {name: 'ab', age: '1', value: 10},
-          {name: 'abc', age: '2', value: 11},
-          {name: 'abcd', age: '3', value: 5},
-          {name: 'abcd', age: '4', value: 5},
-          {name: 'abcd', age: '5', value: 5},
-          {name: 'abcd', age: '6', value: 5},
-          {name: 'abcd', age: '7', value: 5},
-          {name: 'abcd', age: '8', value: 5},
-          {name: 'abcd', age: '9', value: 5},
-          {name: 'abcd', age: '10', value: 5},
-          {name: 'abcd', age: '11', value: 5},
-          {name: 'abcd', age: '12', value: 5},
-          {name: 'abcd', age: '13', value: 5},
-          {name: 'abcd', age: '14', value: 5},
-          {name: 'abcd', age: '15', value: 5},
-          {name: 'abcd', age: '16', value: 5},
-          {name: 'abcd', age: '17', value: 5},
-          {name: 'abcd', age: '18', value: 5},
-          {name: 'abcd', age: '19', value: 5},
-          {name: 'abcd', age: '20', value: 5}
+          {name: 'ab', age: 1, value: 10},
+          {name: 'abc', age: 2, value: 11},
+          {name: 'abcd', age: 3, value: 5},
+          {name: 'abcd', age: 4, value: 5},
+          {name: 'abcd', age: 5, value: 5},
+          {name: 'abcd', age: 6, value: 5},
+          {name: 'abcd', age: 7, value: 5},
+          {name: 'abcd', age: 8, value: 5},
+          {name: 'abcd', age: 9, value: 5},
+          {name: 'abcd', age: 10, value: 5},
+          {name: 'abcd', age: 11, value: 5},
+          {name: 'abcd', age: 12, value: 5},
+          {name: 'abcd', age: 13, value: 5},
+          {name: 'abcd', age: 14, value: 5},
+          {name: 'abcd', age: 15, value: 5},
+          {name: 'abcd', age: 16, value: 5},
+          {name: 'abcd', age: 17, value: 5},
+          {name: 'abcd', age: 18, value: 5},
+          {name: 'abcd', age: 19, value: 5},
+          {name: 'abcd', age: 20, value: 5}
         ]
 
         const totalItems = items.length
-        const { page, rowsPerPage } = this.pagination
-        console.log('rowsPerPage', rowsPerPage)
+        const { sortBy, descending, page, rowsPerPage } = this.pagination
+        console.log('this.pagination', this.pagination)
 
-        // BE magic
+        // BE sorting
+        if (this.pagination.sortBy) {
+          items = items.sort((a, b) => {
+            const sortA = a[sortBy]
+            const sortB = b[sortBy]
+
+            if (descending) {
+              if (sortA < sortB) return 1
+              if (sortA > sortB) return -1
+              return 0
+            } else {
+              if (sortA < sortB) return -1
+              if (sortA > sortB) return 1
+              return 0
+            }
+          })
+        }
+
+        // BE paging
         if (rowsPerPage > 0) {
           items = items.slice((page - 1) * rowsPerPage, page * rowsPerPage)
         }
-        console.log('items', items)
 
         setTimeout(() => {
           this.loading = false
