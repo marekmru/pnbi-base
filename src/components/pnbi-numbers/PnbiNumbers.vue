55f08eea4a69310451ea51edd98513f39dd72374
Mrugalla, Markus <M.Mrugalla@plan-net.com>
2018-10-15 11:22:41 +0200

    Vee Validation in pnbi-numbers

--- src/components/pnbi-numbers/PnbiNumbers.vue
+++ src/components/pnbi-numbers/PnbiNumbers.vue
@@ -1,27 +1,60 @@
 <template>
   <div class="fn-input">
-    <v-text-field :disabled="disabled" :label="label" autocomplete="off" v-model="internalValue" @keydown.native.stop="onKeyDown" :suffix="internalSuffix"></v-text-field>
-    <slot name="validation-slot">
-    </slot>
+    <v-text-field :disabled="disabled" :label="label" autocomplete="off" v-model="internalValue" @keydown.native.stop="onKeyDown" :error-messages="errorMessages" :error="error"
+      :suffix="internalSuffix"></v-text-field>
   </div>
 </template>
 
 <script>
 import is from 'is'
 export default {
+  $_veeValidate: {
+    // fetch the current value from the innerValue defined in the component data.
+    value () {
+      return this.value
+    },
+    name () {
+      return this.label
+    }
+  },
   props: {
-    value: { type: Number, default: null },
-    label: { type: String },
-    valid: { type: Boolean, default: true },
-    type: { type: String, default: 'currency' },
-    suffix: { type: String, default: '€' },
-    unit: { type: Number, required: false, default: 1 },
-    disabled: { type: Boolean, default: false }
+    value: {
+      type: Number,
+      default: null
+    },
+    label: {
+      type: String
+    },
+    error: {
+      type: Boolean,
+      default: false,
+      required: false
+    },
+    errorMessages: {
+      type: Array,
+      required: false
+    },
+    type: {
+      type: String,
+      default: 'currency'
+    },
+    suffix: {
+      type: String,
+      default: '€'
+    },
+    unit: {
+      type: Number,
+      required: false,
+      default: 1
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
   },
 
   methods: {
     onKeyDown (event) {
-      console.log('keydown: ', event.keyCode)
       if (event.keyCode === 38) { // up
         this.internalValue = (this.value / this.unit) + this.incrementor
         return false
@@ -33,26 +66,22 @@ export default {
       }
       if (
         (event.keyCode >= 48 && event.keyCode <= 57) ||
-        (event.keyCode >= 96 && event.keyCode <= 105) ||
-        event.keyCode === 8 ||
-        event.keyCode === 9 ||
-        event.keyCode === 37 ||
-        event.keyCode === 39 ||
-        event.keyCode === 46 ||
-        event.keyCode === 110 ||
-        event.keyCode === 188 ||
-        event.keyCode === 35 ||
-        event.keyCode === 36
-      ) {
-      } else {
+          (event.keyCode >= 96 && event.keyCode <= 105) ||
+          event.keyCode === 8 ||
+          event.keyCode === 9 ||
+          event.keyCode === 37 ||
+          event.keyCode === 39 ||
+          event.keyCode === 46 ||
+          event.keyCode === 110 ||
+          event.keyCode === 188 ||
+          event.keyCode === 35 ||
+          event.keyCode === 36
+      ) {} else {
         event.preventDefault()
       }
     }
   },
   computed: {
-    isValid () {
-      return this.valid
-    },
     internalValue: {
       get: function () {
         if (is.number(this.value)) {
@@ -89,7 +118,4 @@ export default {
 </script>
 
 <style scoped>
-.input-group__details_custom {
-  margin-top: -24px;
-}
 </style>
