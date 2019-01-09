9f154765d3006723c6e54ca3e0e999294d76e256
Mrugalla, Markus <M.Mrugalla@plan-net.com>
2019-01-09 10:28:53 +0100

    refactore APIBASE, created second axios instance for core api

--- src/Auth.js
+++ src/Auth.js
@@ -1,48 +1,33 @@
 import axios from 'axios'
 import debug from './$debug'
 import iHelper from './internal/$internal'
-/* const api = () => {
-  return Vue.prototype.$store.getters.api
-} */
+import {axiosInternal} from './internal/axios.config.js'
 export default {
   login (user) {
-    return axios
+    return axiosInternal
       .post('/login', user)
       .then(result => {
         window.localStorage.setItem('user', JSON.stringify(result.data))
         return result.data
       })
       .catch(error => Promise.reject(error))
-    /*     return Promise.all([login]).then(() => {
-      return this.profile()
-    }) */
   },
   profile () {
     if (this.$profile != null) {
       return Promise.resolve(this.$profile)
     }
-    return axios
+    return axiosInternal
       .get(`/profile`)
       .then(result => {
-        /*         const rn = result.data.result.realname
-        let short = null
-        if (rn.includes(' ')) {
-          short = rn.split(' ').map(val => val.charAt(0).toUpperCase()).join('')
-        } else {
-          short = rn.substring(0, 1)
-        } */
         this.$profile = Object.assign(result.data, {
           short: iHelper.shortName(result.data.realname)
         })
-        // EventBus.$emit(PROFILE_UPDATED, this.$profile)
         return this.$profile
       })
       .catch(error => Promise.reject(error))
   },
   logout () {
-    /*     EventBus.$emit(PROFILE_UPDATED, undefined)
-    this.$profile = undefined */
-    return axios
+    return axiosInternal
       .get(`/logout`)
       .then(result => {
         return result.data
@@ -51,7 +36,7 @@ export default {
   },
   password (password) {
     return debug.notImplemented('password')
-    /*     return axios
+    /*     return axiosInternal
       .post(`https://bi.plan-net.com/api/v2/password`, password)
       .then(result => result.data.result)
       .catch(error => Promise.reject(error)) */
@@ -59,13 +44,13 @@ export default {
   reset (data) {
     // resetten mit token und neuem passwort
     if (data.token != null) {
-      return axios
+      return axiosInternal
         .post(`/login`, data)
         .then(result => result.data)
         .catch(error => Promise.reject(error))
     }
     // anfordern mit email
-    return axios
+    return axiosInternal
       .put(`/login`, data)
       // .put(`/login?email=${data.email}`)
       .then(result => result)
--- src/internal/axios.config.js
+++ src/internal/axios.config.js
@@ -1,79 +1,77 @@
 import axios from 'axios'
-// import EventBus, { ERROR } from '../event-bus'
 import router from './routes/index.js'
 import Vue from 'vue'
+export const axiosInternal = axios.create({
+  baseURL: window.APIBASE_CORE
+})
+axios.defaults.baseURL = window.APIBASE_APP
 
 export function setAjaxConfig (config) {
-  const isApiBaseDefined = window.location.href.includes('localhost') === false
-  // (window.BIAPIBASE != null) && !window.BIAPIBASE.includes('echo var')
-
-  const api = isApiBaseDefined ? (window.BIAPIBASE || window.alert('Error:define window.BIAPIBASE')) : `http://localhost:${config.FALLBACK}`
-
-  axios.defaults.baseURL = api
-  axios.defaults.headers.common['Accept'] = 'application/json'
-
+  //helpers
   const ingnoredErrors = config.IGNORED_ERRORS || []
   const ignoredEndpoints = ['/logout', '/profile']
   const current = router.instance.history.current
   function isErrorIgnoreRoute () {
-    /*     console.log(history.current)
-    if (history.current.path == null) {
-      return true
-    } */
     return router.publicPages.indexOf(current.path) > -1
   }
-  axios.interceptors.response.use(
-    response => {
-      return response.data
-    },
-    error => {
-      const isIgnoredEndopoint = ignoredEndpoints.indexOf(
-        error.response.request.responseURL
-      ) > -1
-      /*       const isIgnoredEndopoint =
-        error.response.request.responseURL.includes('/logout') ||
-        error.response.request.responseURL.includes('/profile') */
-
-      if (isErrorIgnoreRoute()) { // pass
-      } else {
-        let data = {}
-        // error without corret body > fallback to defaults
-        // 500
-        if (error.response == null) {
-          data.status_code = 500
-          data.html = 'Ein Server Fehler ist aufgetreten.<br>Bitte kontaktieren sie uns unter: <a href="mailto:bi-ops@plan-net.com">bi-ops@plan-net.com</a>'
-        } else if (
-          ingnoredErrors.indexOf(error.response.status) === -1 &&
-          isIgnoredEndopoint === false
-        ) {
-          data.status_code = error.response.status
-          if (data.status_code === 401) {
-            data.html = 'Bitte loggen sie sich ein.'
-          } else if (data.status_code === 403) {
-            data.html =
-              'Sie sind nicht authorisiert, diese Ressource aufzurufen.<br>Bitte kontaktieren sie uns unter: <a href="mailto:bi-ops@plan-net.com">bi-ops@plan-net.com</a>'
-          } else {
-            data.json = error.response.data || error.response
+  // AXIOS //
+  // used in pnbi-base client
+  // used internally by Auth.js because Auth Path is different then api above
+  const instances = [axios, axiosInternal]
+  instances.forEach( axiosInstance => {
+    axiosInstance.defaults.headers.common['Accept'] = 'application/json'
+    axiosInstance.interceptors.response.use(
+      response => {
+        return response.data
+      },
+      error => {
+        const isIgnoredEndopoint = ignoredEndpoints.indexOf(
+          error.response.request.responseURL
+        ) > -1
+  
+        if (isErrorIgnoreRoute()) { // pass
+        } else {
+          let data = {}
+          // error without corret body > fallback to defaults
+          // 500
+          if (error.response == null) {
+            data.status_code = 500
+            data.html = 'Ein Server Fehler ist aufgetreten.<br>Bitte kontaktieren sie uns unter: <a href="mailto:bi-ops@plan-net.com">bi-ops@plan-net.com</a>'
+          } else if (
+            ingnoredErrors.indexOf(error.response.status) === -1 &&
+            isIgnoredEndopoint === false
+          ) {
+            data.status_code = error.response.status
+            if (data.status_code === 401) {
+              data.html = 'Bitte loggen sie sich ein.'
+            } else if (data.status_code === 403) {
+              data.html =
+                'Sie sind nicht authorisiert, diese Ressource aufzurufen.<br>Bitte kontaktieren sie uns unter: <a href="mailto:bi-ops@plan-net.com">bi-ops@plan-net.com</a>'
+            } else {
+              data.json = error.response.data || error.response
+            }
+            // EventBus.$emit(ERROR, data)
+            Vue.prototype.$store.dispatch('showError', data)
           }
-          // EventBus.$emit(ERROR, data)
-          Vue.prototype.$store.dispatch('showError', data)
         }
+        return Promise.reject(error.response.data)
       }
-      return Promise.reject(error.response.data)
-    }
-  )
-  axios.interceptors.request.use(
-    request => {
-      if (!request.url.includes('/login')) {
-        const user = JSON.parse(window.localStorage.getItem('user'))
-        if (user != null) {
-          request.headers.Authorization = `Bearer ${user.token}`
+    )
+    axiosInstance.interceptors.request.use(
+      request => {
+        if (request.url.includes('/login') === false) {
+          const user = JSON.parse(window.localStorage.getItem('user'))
+          if (user != null) {
+            request.headers.Authorization = `Bearer ${user.token}`
+          }
         }
+        return request
+      },
+      error => {
+        return Promise.reject(error)
       }
-      return request
-    },
-    error => {
-      return Promise.reject(error)
-    }
-  )
+    )
+  })
+
+
 }
