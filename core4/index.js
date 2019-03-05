f3980deecf4f4dcdb807087e874e5a97db03c11c
Mrugalla, Markus <M.Mrugalla@plan-net.com>
2019-03-05 13:56:14 +0100

    cleanup datatable plus

--- core4/index.js
+++ core4/index.js
@@ -9,7 +9,7 @@ import { setAjaxConfig } from './internal/axios.config.js'
 import { setCookieConfig } from './internal/cookie.service.js'
 import { setStore } from './store'
 import PnbiDataTable from './components/pnbi-table/PnbiTable'
-import PnbiDataTablePlus from './components/pnbi-table-plus/PnbiTablePlus'
+// import PnbiDataTablePlus from './components/pnbi-table-plus/PnbiTablePlus'
 import PnbiEmpty from './components/pnbi-empty/PnbiEmpty'
 import bus from './event-bus'
 import helper from './helper'
@@ -80,40 +80,9 @@ const install = (Vue, options) => {
   Vue.component('pnbi-card', PnbiCard)
   Vue.component('pnbi-page', PnbiPage)
   Vue.component('pnbi-datatable', PnbiDataTable)
-  Vue.component('pnbi-datatable-plus', PnbiDataTablePlus)
+  // Vue.component('pnbi-datatable-plus', PnbiDataTablePlus)
   Vue.component('pnbi-webapp', PnbiWebapp)
   Vue.component('pnbi-empty', PnbiEmpty)
-  /*
-  * Use this filter for custom formating includes local settings:
-  * numbers – with numbro.js
-  * datetime – with moment.js
-  *
-  * FORMAT
-  * @param key = {style:'numbro.js', format:'0,0.00'}
-  */
-  /* #####################
- * mmr: Please choose a meaningful name : FE datatablePlusXYZ Fromatter
- ##################### */
-  Vue.filter('customFormatter', function (value, key) {
-    if (!value) return ''
-    switch (key.style) {
-      case 'numbro.js':
-        if (key.format) {
-          value = numbro(value).format(key.format)
-        } else {
-          value = numbro(value).format('0,0')
-        }
-        break
-      case 'moment.js':
-        if (key.format) {
-          value = moment(value).format(key.format)
-        } else {
-          value = moment(value).format('l')
-        }
-        break
-    }
-    return value
-  })
 
   /* TODO: possibility to pass a skin */
   Vue.use(Vuetify, {
