97ffa14263e277e3cea0f92b1e5710b365f127e3
Mrugalla, Markus <M.Mrugalla@plan-net.com>
2020-07-21 11:05:50 +0200

    cache buster for index.html

---
+++ src/version.js
@@ -0,0 +1,42 @@
+var fs = require('fs')
+
+/* fs.readFile('../../__init__.py', 'utf8', function (err, data) {
+  if (err) throw err
+  data = data.split("'").join('"')
+
+  const s1 = data.substring(data.indexOf('name = ') + 7)
+  const name = s1.substring(0, s1.indexOf('title')).split('"').join('').replace('\n', '')
+
+  const s3 = data.substring(data.indexOf('__version__ =') + 14)
+  const version = s3.substring(0, s3.indexOf('__built__')).split('"').join('').replace('\n', '')
+  const all = name + '/' + version
+  const date = new Date()
+
+  fs.readFile('./dist/index.html', 'utf8', function (err, data) {
+    if (err) throw err
+    const tmp = data.split('</head>')
+    const version = '<script>window.__VERSION__ ="v.'  + all + '"; window.__CACHEB__ = "'+date+'"</script></head>'
+
+    const res = tmp[0] + version + tmp[1]
+
+    fs.writeFile('./dist/index.html', res, function (err) {
+      if (err) {
+        return console.log(err)
+      }
+    })
+  })
+}) */
+fs.readFile('./dist/index.html', 'utf8', function (err, data) {
+  if (err) throw err
+  const tmp = data.split('</head>')
+  const version = 'window.__CACHEB__ = "'+new Date()+'"</script></head>'
+
+  const res = tmp[0] + version + tmp[1]
+
+  fs.writeFile('./dist/index.html', res, function (err) {
+    if (err) {
+      return console.log(err)
+    }
+  })
+})
+
